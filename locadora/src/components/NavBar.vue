bf397a66db49fbb9c771ec3b2a5df1858195be83
FabioNog <fabio.carmo@computacao.ufla.br>
2020-01-24 13:22:19 -0300

    Add vue-router and buildin my router for components

--- locadora/src/components/NavBar.vue
+++ locadora/src/components/NavBar.vue
@@ -1,25 +1,43 @@
 <template>
   <div>
   <b-navbar toggleable="lg" type="dark  " variant="info">
-    <b-navbar-brand href="#">Locadora</b-navbar-brand>
+    <b-navbar-brand href="#">
+    <router-link to=/>Locadora</router-link>
+    </b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     
 
     <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml">
-        <b-nav-item-dropdown text="Filmes">
-          <b-dropdown-item href="#">Criar Filmes</b-dropdown-item>
-          <b-dropdown-item href="#">Editar Filmes</b-dropdown-item>
-          <b-dropdown-item href="#">Excluir Filmes</b-dropdown-item>
-          <b-dropdown-item href="#">Listar Filmes</b-dropdown-item>
+        <b-nav-item-dropdown text="Filmes">        
+          <b-dropdown-item href="#">
+            <router-link to=/criarfilme>Criar Filmes</router-link>
+          </b-dropdown-item>        
+          <b-dropdown-item href="#">
+            <router-link to=/editarfilme>Editar Filmes</router-link>
+          </b-dropdown-item>
+          <b-dropdown-item href="#">
+            <router-link to=/excluirfilme>Excluir Filmes</router-link>
+          </b-dropdown-item>
+          <b-dropdown-item href="#">
+            <router-link to=/listarfilme>Listar Filmes</router-link>
+          </b-dropdown-item>
         </b-nav-item-dropdown>
 
         <b-nav-item-dropdown text="Lang">
-          <b-dropdown-item href="#">Criar Diretores</b-dropdown-item>
-          <b-dropdown-item href="#">Editar Diretores</b-dropdown-item>
-          <b-dropdown-item href="#">Excluir Diretores</b-dropdown-item>
-          <b-dropdown-item href="#">Listar Diretores</b-dropdown-item>
+          <b-dropdown-item href="#">
+            <router-link to=/criardiretor>Criar Diretores</router-link>
+          </b-dropdown-item>
+          <b-dropdown-item href="#">
+            <router-link to=/editardiretor>Editar Diretores</router-link>
+          </b-dropdown-item>
+          <b-dropdown-item href="#">
+            <router-link to=/excluirdiretor>Excluir Diretores</router-link>
+          </b-dropdown-item>
+          <b-dropdown-item href="#">
+            <router-link to=/listardiretor>Listar Diretores</router-link>
+          </b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>      
     </b-collapse>
--- locadora/src/main.js
+++ locadora/src/main.js
@@ -1,16 +1,51 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-
 //Bootrastrap Framework
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { BootstrapVue } from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 
+//import components Filmes
+import CriarFilme from './components/Filmes/CriarFilme'
+import EditarFilme from './components/Filmes/EditarFilme'
+import ExcluirFilme from './components/Filmes/ExcluirFilme'
+import ListarFilme from './components/Filmes/ListarFilme'
+
+//import components Diretores
+import CriarDiretor from './components/Diretores/CriarDiretor'
+import EditarDiretor from './components/Diretores/EditarDiretor'
+import ExcluirDiretor from './components/Diretores/ExcluirDiretor'
+import ListarDiretor from './components/Diretores/ListarDiretor'
+
+//import Router
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const routes = [
+  //Filmes
+  {path: "/criarfilme",component: CriarFilme},
+  {path: "/editarfilme",component: EditarFilme},
+  {path: "/excluirfilme",component: ExcluirFilme},
+  {path: "/listarfilme",component: ListarFilme},
+  //Diretores
+  {path: "/criardiretor",component: CriarDiretor},
+  {path: "/editardiretor",component: EditarDiretor},
+  {path: "/excluirdiretor",component: ExcluirDiretor},
+  {path: "/listardiretor",component: ListarDiretor},
+]
+
+const router = new VueRouter({
+  routes
+})
+
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
