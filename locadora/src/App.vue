4c439ea8fb5dfe533f4cf5797f9b493b2193a71e
FabioNog <fabio.carmo@computacao.ufla.br>
2020-01-23 21:08:44 -0300

    Alter app.vue

--- locadora/src/App.vue
+++ locadora/src/App.vue
@@ -1,6 +1,27 @@
 <template>
   <div id="app">
     <NavBar/>
+    <hr/>
+    <b-card-group deck>
+      <b-card title="Crie Filmes" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+        <b-card-text>
+          Crie novos filmes, desde os mais novos até os mais antigos.
+        </b-card-text>        
+      </b-card>
+
+      <b-card title="Edite Filmes" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+        <b-card-text>
+          This card has supporting text below as a natural lead-in to additional content.
+        </b-card-text>
+      </b-card>
+
+      <b-card title="Exclua Filmes" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+        <b-card-text>
+          This is a wider card with supporting text below as a natural lead-in to additional content.
+          This card has even longer content than the first to show that equal height action.
+        </b-card-text>
+      </b-card>
+    </b-card-group>
   </div>
 </template>
 
@@ -11,7 +32,8 @@ export default {
   name: 'app',
   components: {
     NavBar
-  }
+  },
+
 }
 </script>
 
--- locadora/src/components/NavBar.vue
+++ locadora/src/components/NavBar.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
-  <b-navbar toggleable="lg" type="light" variant="info">
+  <b-navbar toggleable="lg" type="dark  " variant="info">
     <b-navbar-brand href="#">Locadora</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    
 
     <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#">Criar Filmes</b-nav-item>
-        <b-nav-item href="#">Editar Filmes</b-nav-item>
-        <b-nav-item href="#">Excluir Filmes</b-nav-item>
-        <b-nav-item href="#">Listar Filmes</b-nav-item>
-      </b-navbar-nav>
+        <b-navbar-nav class="ml">
+        <b-nav-item-dropdown text="Filmes">
+          <b-dropdown-item href="#">Criar Filmes</b-dropdown-item>
+          <b-dropdown-item href="#">Editar Filmes</b-dropdown-item>
+          <b-dropdown-item href="#">Excluir Filmes</b-dropdown-item>
+          <b-dropdown-item href="#">Listar Filmes</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown text="Lang">
+          <b-dropdown-item href="#">Criar Diretores</b-dropdown-item>
+          <b-dropdown-item href="#">Editar Diretores</b-dropdown-item>
+          <b-dropdown-item href="#">Excluir Diretores</b-dropdown-item>
+          <b-dropdown-item href="#">Listar Diretores</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>      
     </b-collapse>
   </b-navbar>
 </div>
@@ -28,5 +38,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.navbar{
+  background-color: black !important;
+}
+.navbar .navbar-brand{
+    color: white !important;
+}
 
 </style>
